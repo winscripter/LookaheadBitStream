45072bd55baf42a8f44c9782c035bdc4175267b6
winscripter <142818255+winscripter@users.noreply.github.com>
2026-02-25 21:49:36 +0400

    New lookahead tests

--- Library.Tests/LookaheadBitStreamTests.cs
+++ Library.Tests/LookaheadBitStreamTests.cs
@@ -19,4 +19,24 @@ public class LookaheadBitStreamTests
         Assert.Equal(0, bs.ReadBits(5));
         Assert.Equal(5, bs.ReadBits(3));
     }
+
+    [Fact]
+    public void TestBitLookahead()
+    {
+        var ms = new MemoryStream([0b00000011, 0b00000010, 0b00000101])
+        {
+            Position = 0
+        };
+
+        var bs = new LookaheadBitStream(ms);
+        Assert.Equal(3, bs.PeekBits(8));
+        Assert.Equal(3, bs.ReadBits(8));
+        bs.ReadBits(4);
+        Assert.Equal(0, bs.ReadBits(2));
+        Assert.Equal(1, bs.PeekBits(1));
+        Assert.Equal(2, bs.ReadBits(2));
+
+        Assert.Equal(0, bs.ReadBits(5));
+        Assert.Equal(5, bs.ReadBits(3));
+    }
 }
